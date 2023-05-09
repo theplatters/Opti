8c321ce145bb49c818d94a93ee170c539a5fb099
theplatters <fscharnreitner@gmail.com>
2023-05-09 21:36:53 +0200

    hessians

---
+++ f_aH.m
@@ -0,0 +1,8 @@
+function [f_val, g, exit_flag] = f_aH(x)
+    exit_flag=0;
+    x1=x(1);
+    x2=x(2);
+    f_val = 0.5*(x1+x2)^2 + 0.05*(x1-x2)^2;
+    g = [1.1*x1+0.9*x2;0.9*x1+1.1*x2];
+    H = [1.1,0.9;0.9,0.1];
+end
---
+++ f_cH.m
@@ -0,0 +1,17 @@
+function [f_val, g, H] = f_cH(x)
+    n = length(x);
+    if min(x)<=0
+        f_val = 69;
+        g = 420*ones(n,1);
+        %disp("Function evaluation at invalid input, function value set to dummy, abort computation")
+        return
+    end
+    
+    f_val = sum((1:n).*x.*reallog(x)) + 1/sum(x);
+    g=zeros(n,1);
+    for i=1:n
+        g(i)=i*(reallog(x(i))+1)-1/sum(x)^2;
+    end
+    h = 2/(sum(x))^3;
+    H = h*ones(n,n)+diag((1:n)./x',0);
+end
