872454e51a696c60c89024cce6ccd1dcde843570
theplatters <fscharnreitner@gmail.com>
2023-05-10 17:26:05 +0200

    comment in st.desc

--- SteepestDescent.m
+++ SteepestDescent.m
@@ -1,7 +1,9 @@
 function [x,f_val,g,exit_flag, iter, evals] = SteepestDescent (f, x0, phi_min,eps,itmax, typ_f,typ_x)
-
+  %Steepest Descent Algorithm for testing LineSearch
+  
+  %for varible input arguments
   if nargin < 7
-    typ_x(1:size(x0,1)) = 10^-4;
+    typ_x(1:length(x0)) = 10^-4;
   end
 
   if nargin < 6
@@ -12,10 +14,6 @@ function [x,f_val,g,exit_flag, iter, evals] = SteepestDescent (f, x0, phi_min,ep
     itmax = 1000;
   end
 
-   if nargin < 5
-    itmax = 1000;
-  end
-
   if nargin < 4
     eps = 10^-6;
   end
@@ -23,28 +21,35 @@ function [x,f_val,g,exit_flag, iter, evals] = SteepestDescent (f, x0, phi_min,ep
   if nargin < 3
     phi_min = -10^30;
   end
-
+  
+  %starting value for iteration, function value, gradient, exit_flag
   xk = x0;
   [fk,gk,exit_flag] = f(x0);
   evals = 1;
+  
+  %iterations of steepest descent
   for iter = 1:itmax
-
-    if max(abs(gk) .* typ_x / typ_f) <= eps
+      
+    %termination condition fullfilled (relative gradient less than tolerance)
+    if max(abs(gk) .* typ_x / typ_f) <= eps  
       x = xk;
       f_val = fk;
       g = gk;
       return
     end
-
+    
+    %calling LineSearch
     [xk,fk,gk,exit_flag,~,eval_temp] = LineSearch(f,xk,fk,gk,-gk,phi_min);
     typ_f = max(typ_f,abs(fk));
     typ_x = max(typ_x,abs(xk));
-    evals = evals + eval_temp;
+    evals = evals + eval_temp; %updating number of evaluations 
   end
-
-      x = xk;
-      f_val = fk;
-      g = gk;
-      exit_flag = 1;
+  
+  %Setting output, if termination condition is not fullfilled and maximal
+  %number of iterations reached
+  x = xk;
+  f_val = fk;
+  g = gk;
+  exit_flag = 1;
 
 end
