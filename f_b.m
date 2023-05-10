fc5670930f33600840c88e2221f936dacbe542a2
theplatters <fscharnreitner@gmail.com>
2023-05-10 17:22:57 +0200

    maxs new functions

--- f_b.m
+++ f_b.m
@@ -3,6 +3,5 @@ function [f_val, g, exit_flag] = f_b(x)
     x1=x(1);
     x2=x(2);
     f_val = 100*(x2-x1^2)^2 + (1-x1)^2;
-    gradient = @(x,y)[400*x*(x^2-y)+2*(x-1);200*(y-x^2)];
-    g=gradient(x1,x2);
+    g = [400*x1*(x1^2-x2) + 2*(x1-1); 200*(x2-x1^2)];
 end
--- f_cH.m
+++ f_cH.m
@@ -1,17 +1,20 @@
 function [f_val, g, H] = f_cH(x)
     n = length(x);
-    if min(x) <= 0
-        f_val = 10^30;
-        g = 420*ones(n,1);
-        %disp("Function evaluation at invalid input, function value set to dummy, abort computation")
+    if min(x)<=0
+        f_val = 69; %dummy values
+        g = 420*ones(n,1); %dummy values
         return
     end
     
-    f_val = sum((1:n).*x.*reallog(x)) + 1/sum(x);
-    g=zeros(n,1);
+    x=x(:); %transform to column vector
+    %(1:n)', x, log(x) column vectors
+    f_val = sum((1:n)'.*x.*log(x)) + 1/sum(x);
+    g = zeros(n,1); %column vector
     for i=1:n
-        g(i)=i*(reallog(x(i))+1)-1/sum(x)^2;
+        g(i) = i*(reallog(x(i))+1) - 1/sum(x)^2;
     end
-    h = 2/(sum(x))^3;
-    H = h*ones(n,n)+diag((1:n)./x',0);
+    if nargout > 2
+        h = 2/sum(x)^3;
+        H = h*ones(n,n) + diag((1:n)'./x,0);
+    end
 end
