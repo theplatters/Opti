823f2388c84d4d3d0d58df6d9f96f8e6e8036f39
theplatters <fscharnreitner@gmail.com>
2023-05-11 20:42:33 +0200

    ex2

--- src/Prog1Ex2.m
+++ src/Prog1Ex2.m
@@ -3,120 +3,94 @@ if exist(dfile1, 'file') ; delete(dfile1); end
 diary(dfile1)
 diary on
 
-options1= optimset('LargeScale','off','GradObj','on'); 
-options2= optimset('LargeScale','on','GradObj','on','Hessian','off');
-options3= optimset('Algorithm','trust-region','LargeScale','on','GradObj','on','Hessian','on');
+options1= optimset('LargeScale','off','GradObj','on','Display','off'); 
+options2= optimset('LargeScale','on','GradObj','on','Hessian','off','Display','off');
+options3= optimset('Algorithm','trust-region','LargeScale','on','GradObj','on','Hessian','on','Display','off');
 options = [options1,options2,options3];
 
 %(a)
-disp("===========================================")
+disp("======================================================")  
 disp("(A)")
-disp("===========================================")
+disp("======================================================")  
 for i=1:length(options)
     disp("Testing objective function f_a with options: ")
     disp(options(i));
-    
-    s_a = fminunc(@f_aH,[10,-3],options(i));
+    [x,f,~,out,g] = fminunc(@f_aH,[10,-3],options(i));
     disp("Calculated minimizer: ");
-    s_a
-    [fm,g] = f_aH(s_a);
-    disp("Calculated Minimum value: " + fm);
+    x
+    disp("Calculated Minimum value: " + f);
     disp("Norm of gradient at calculated Minimum: " + norm(g));
-    disp("===========================================")    
+    disp(out);
+    disp("======================================================")    
 end
 
 
-disp("===========================================")
+disp("======================================================")
 disp("(B)")
-disp("===========================================")
+disp("======================================================")
 for i=1:length(options)
     disp("Testing objective function f_b with options: ")
     disp(options(i));
     
-    s_b = fminunc(@f_bH,[-1,2],options(i));
+    [x,f,~,out,g] = fminunc(@f_bH,[-1,2],options(i));
     disp("Calculated minimizer: ");
-    s_b
-    
-    [fm,g] = f_bH(s_b);
-    disp("Calculated Minimum value: " + fm);
+    x
+    disp("Calculated Minimum value: " + f);
     disp("Norm of gradient at calculated Minimum: " + norm(g));
-    disp("===========================================")
+    disp(out)
+    disp("======================================================")
 
 end
     
 
-disp("===========================================")
+disp("======================================================")
 disp("C")
-disp("===========================================")
+disp("======================================================")
 
 for i=1:length(options)
     disp("Testing objective function f_c with options: ")
     disp(options(i));
     
-    disp("===========================================") 
-    disp("Results for n=10")
-    s_c1 = fminunc(@f_cH,5*ones(1,10),options(i));
+    disp("======================================================")
+    for j=1:3
+        n=10^j;
+        disp("Results for n= "+n);
+        [x,f,~,out,g] = fminunc(@f_cH,5*ones(1,n),options(i));
+        if j==1
+            disp("Calculated minimizer: ");
+            x
+        end
+        disp("Calculated Minimum value: " + f);
+        disp("Norm of gradient at calculated Minimum: " + norm(g));
+        disp(out)
+        disp("====================================================")
+    end
     
-    disp("Calculated minimizer: ");
-    s_c1
-    [fm,g] = f_cH(s_c1);
-    disp("Calculated Minimum value: " + fm);
-    disp("Norm of gradient at calculated Minimum: " + norm(g));
-    
-    disp("===========================================")
-    disp("Results for n=100")
-    s_c2 = fminunc(@f_cH,5*ones(1,100),options(i));
-    [fm,g] = f_cH(s_c2);
-    
-    disp("Calculated Minimum value: " + fm);
-    disp("Norm of gradient at calculated Minimum: " + norm(g));
-    
-    disp("===========================================")
-    disp("Results for n=1000")
-    s_c3 = fminunc(@f_cH,5*ones(1,1000),options(i));
-    [fm,g] = f_cH(s_c3);
-    
-    disp("Calculated Minimum value: " + fm);
-    disp("Norm of gradient at calculated Minimum: " + norm(g));
 end
 
-
-
-disp("===========================================")
+disp("====================================================")
 disp("(D)")
-disp("===========================================")
-
+disp("====================================================")
 for i=1:length(options)
     disp("Testing objective function f_d with options: ")
     disp(options(i));
     
-    disp("===========================================")
-    disp("Results for n=10")
-    s_d1 = fminunc(@f_dH,[zeros(1,9),100],options(i));
-    disp("Calculated minimizer: ") 
-    s_d1
+    disp("======================================================")
+    for j=1:3
+        n=10^j;
+        disp("Results for n= "+n);
+        [x,f,~,out,g] = fminunc(@f_dH,[zeros(1,n-1),10*n],options(i));
+        if j==1
+            disp("Calculated minimizer: ");
+            x
+        end
+        disp("Calculated Minimum value: " + f);
+        disp("Norm of gradient at calculated Minimum: " + norm(g));
+        disp(out)
+        disp("====================================================")
+    end
     
-    [fm,g] = f_dH(s_d1);
-    disp("Calculated Minimum value: " + fm);
-    disp("Norm of gradient at calculated Minimum: " + norm(g));
-    
-    disp("===========================================")
-    disp("Results for n=100")
-    s_d2 = fminunc(@f_dH,[zeros(1,99),1000],options(i));
-    [fm,g] = f_dH(s_d2);
-    
-    disp("Calculated Minimum value: " + fm);
-    disp("Norm of gradient at calculated Minimum: " + norm(g));
-    
-    disp("===========================================")
-    disp("Results for n=1000")
-    s_d3 = fminunc(@f_dH,[zeros(1,999),10000],options(i));
-    [fm,g] = f_dH(s_d3);
-    
-    disp("Calculated Minimum value: " + fm);
-    disp("Norm of gradient at calculated Minimum: " + norm(g));
 end
-
 
 diary off
 
