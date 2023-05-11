ed427013e2af8eebd9f3fa6d94e6d0129446d2f9
hni2002 <133199114+hni2002@users.noreply.github.com>
2023-05-11 09:02:33 +0200

    Update Prog1Ex4.m

--- src/Prog1Ex4.m
+++ src/Prog1Ex4.m
@@ -1,8 +1,10 @@
-
 options1 = optimoptions('fminunc','GradObj','off');
 options2 = optimset('GradObj','off');
 
 f = @(x) x(1)+10*max(x(1)^2+2*x(2)^2-1,0);
 x0=[1,1];
-s1=fminunc(f,x0,options1)
+s1 = fminunc(f,x0,options1)
 s2 = fminsearch(f,x0,options2)
+
+e1 = norm(s1-[-1,0])
+e2 = norm(s2-[-1,0])
