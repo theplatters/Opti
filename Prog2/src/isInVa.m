009e8a245bd92e852e208489f1758de32bfbd610
theplatters <fscharnreitner@gmail.com>
2023-06-23 14:25:45 +0200

    fix: first (non)working version

--- Prog2/src/isInVa.m
+++ Prog2/src/isInVa.m
@@ -7,10 +7,9 @@ isIn = false;
 
 v = b + mu * beta - A' * x;
 w = G * x + d + mu * c + A * lambda;
-s = sum((lambda .* (v) ./ mu - 1) .^ 2):
+s = sum( (lambda .* v ./ mu - 1) .^ 2);
 
-
-if  v > 0 && w == 0 && lambda > 0  && mu > 0 && s <= alpha^2
+if  all(v > 0) && all(w == 0) && all(lambda > 0)  && mu > 0 && s <= alpha^2
     isIn = true;
 end
 
