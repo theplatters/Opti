d9ad94480e4f605cc65f673eade72cdef9ebc927
theplatters <fscharnreitner@gmail.com>
2023-06-29 13:40:39 +0200

    testing

--- Prog2/src/ex5Test.m
+++ Prog2/src/ex5Test.m
@@ -22,13 +22,15 @@ n = 9;
     n = length(x0);
 
     [x,lambda,exitflag, iter] = interiorpoint(x0,mu0,G,d,A,b,alpha);
-
+    x2 = fmincon(@(x) 0.5 *x' * G * x + d'*x,x0,A',b);
+   
     st = sprintf("after %d iterations",iter);
-
+    st2 = sprintf("difference from solution found with fmincon: %d",max(abs(x - x2)));
     if(exitflag == 0)
         disp("Converged to")
         disp(x)
         disp(st);
+        disp(st2)
     else
         disp("Not converged")
         disp(x)
